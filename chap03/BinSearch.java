18d4a0e061f185d96060c3223cd48b3cbae9b5ee
00nahyeonKim <nahyeon000918@gmail.com>
2026-06-09 19:14:02 +0900

    Initial commit

---
+++ chap03/BinSearch.java
@@ -0,0 +1,52 @@
+package chap03;
+// 이진 검색
+
+import java.util.Scanner;
+
+public class BinSearch {
+    
+    // 요솟수가 n개인 배열 a에서 key와 같은 요소를 이진 검색
+    static int BinSearch(int[] a, int n, int key) {
+        int pl = 0;
+        int pr = n - 1;
+
+        do {
+            int pc = (pl + pr) / 2; // 중앙 요소의 인덱스
+            if (a[pc] == key) return pc;
+            
+            else if (a[pc] < key) pl = pc + 1;
+
+            else pr = pc - 1;
+        } while (pl <= pr);
+
+        return -1; // 검색 실패
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("요솟수: ");
+        int num = sc.nextInt();
+        int[] x = new int[num];
+
+        System.out.println("오른차순으로 입력하세요.");
+
+        System.out.print("x[0]: ");
+        x[0] = sc.nextInt();
+
+        for (int i = 1; i < num; i++) {
+            do {
+                System.out.print("x[" + i + "]: ");
+                x[i] = sc.nextInt();
+            } while(x[i] < x[i-1]);
+        }
+
+        System.out.print("검색할 값: ");
+        int ky = sc.nextInt();
+
+        int idx = BinSearch(x, num, ky);
+
+        if (idx == -1) System.out.println("그 값의 요소가 없습니다.");
+        else System.out.println("그 값은 x[" + idx + "]에 있습니다.");
+    }
+}
